da55c1d489b2aad4ea6f6231e52764b679dfc277
Kashif <l174213@lhr.nu.edu.pk>
2024-07-18 04:38:42 +0500

    React Event Planner

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+
+const Footer = () => {
+    return (
+        <>
+            <footer>
+                <p> Event Planner Organization. All rights reserved.</p>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
